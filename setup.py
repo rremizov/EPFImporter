fb763d36e94f9a2c3055d20b5d476c24a1ee9cf2
rremizov <rremizov@yandex.ru>
2014-01-22 16:43:52 +0600

    Revert "rename: EPFImporter -> epfimporter"
    This reverts commit 464484eff44c10143e193ba0b10fb2803399d07d.

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup, find_packages
 from os.path import join, dirname
 
-import epfimporter
+import EPFImporter
 
 
 setup(
-    name='epfimporter',
-    version=epfimporter.__version__,
+    name='EPFImporter',
+    version=EPFImporter.__version__,
     packages=find_packages(),
     long_description=open(join(dirname(__file__), 'README.md')).read(),
 )
